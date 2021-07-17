0d63af4a773d704c0bf15842d1e065faf213be65
jaydaftari <jaydaftari19@gmail.com>
2021-07-17 19:00:39 +0530

    initial commit.

---
+++ src/components/App.js
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from 'react';
+import CustomEditor from './CustomEditor'
+import useLocalStorage from '../hooks/useLocalStorage'
+
+function App() {
+  const [html, setHtml] = useLocalStorage('html', '')
+  const [css, setCss] = useLocalStorage('css', '')
+  const [js, setJs] = useLocalStorage('js', '')
+  const [srcDoc, setSrcDoc] = useState('')
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setSrcDoc(`
+        <html>
+          <body>${html}</body>
+          <style>${css}</style>
+          <script>${js}</script>
+        </html>
+      `)
+    }, 250)
+
+    return () => clearTimeout(timeout)
+  }, [html, css, js])
+
+  return (
+    <>
+      <div className="pane top-pane">
+       
+        <CustomEditor
+          language="css"
+          displayName="index.css"
+          value={css}
+          onChange={setCss}
+        />
+         <CustomEditor
+          language="xml"
+          displayName="index.html"
+          value={html}
+          onChange={setHtml}
+        />
+        <CustomEditor
+          language="javascript"
+          displayName="index.js"
+          value={js}
+          onChange={setJs}
+        />
+      </div>
+      <div className="pane">
+        <iframe
+          srcDoc={srcDoc}
+          title="output"
+          sandbox="allow-scripts"
+          frameBorder="0"
+          width="100%"
+          height="100%"
+        />
+      </div>
+    </>
+  )
+}
+
+export default App;
