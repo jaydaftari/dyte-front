998572d9c49589cf08325fb50c1c36f688655db3
jaydaftari <jaydaftari19@gmail.com>
2021-07-17 19:38:18 +0530

    Updates

---
+++ precache-manifest.3043a0552aa6a43d634d290d0bb1a5af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c27031d49c99d2b49c99734e5dd779fd",
+    "url": "/jaydaftari/dyte-front/index.html"
+  },
+  {
+    "revision": "7231aaf7544d70a4ca02",
+    "url": "/jaydaftari/dyte-front/static/css/2.61721385.chunk.css"
+  },
+  {
+    "revision": "b83ba18fab9869f4801b",
+    "url": "/jaydaftari/dyte-front/static/css/main.7d2118c4.chunk.css"
+  },
+  {
+    "revision": "7231aaf7544d70a4ca02",
+    "url": "/jaydaftari/dyte-front/static/js/2.a466877c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jaydaftari/dyte-front/static/js/2.a466877c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b83ba18fab9869f4801b",
+    "url": "/jaydaftari/dyte-front/static/js/main.9365b1a6.chunk.js"
+  },
+  {
+    "revision": "9532567f3e94f2263302",
+    "url": "/jaydaftari/dyte-front/static/js/runtime-main.cef123cb.js"
+  }
+]);
